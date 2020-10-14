a8bdef2f004a56a64898468bf038397a399e14d5
George Owen <gbo.owen@gmail.com>
2020-10-13 19:42:26 -0700

    A Commit from my Ubuntu Laptop Distro, on Tue 13 Oct 2020 07:42:26 PM PDT

--- OCaml/hw1test.ml
+++ OCaml/hw1test.ml
@@ -1,3 +1,63 @@
+
+
+let setA = [1;1;1;1;];;
+let setB = [1;2;3;4;5;6;7;8;9;10;];;
+
+let setB2 = [1;2;3;4;5;6;7;8;9;10;]
+
+let strSetA = ["a";"b";"c";"d";];;
+let strSetB = ["A";"a";"B";"b";"C";"c";"D";"d";];;
+
+
+(* testing 1. *)
+
+let subset_test0 = subset setA setB
+
+let subset_test1 = subset strSetA strSetB
+
+
+(* testing 2. *)
+
+let equal_sets_test0 = equal_sets setA setA
+
+let equal_sets_test1 = not (equal_sets setA setB)
+
+
+(* testing 3. *)
+
+let set_union_test0 = equal_sets (set_union strSetA strSetB) ["a";"b";"c";"d";"A";"a";"B";"b";"C";"c";"D";"d";]
+
+let set_union_test1 = equal_sets (set_union setA [2;3;4;5]) [1;2;3;4;5]
+
+(* testing 4. *)
+
+let set_intersection_test0 = equal_sets (set_intersection setA setB) [1;]
+
+let set_intersection_test1 = equal_sets (set_intersection setB setB) setB
+
+
+(* testing 5. *)
+
+let set_diff_test0 = equal_sets (set_difference setB setA) [2;3;4;5;6;7;8;9;10]
+
+let set_diff_test1 = equal_sets (set_difference setA setB) []
+
+
+(* testing 6. *)
+
+let computed_fixed_point_test0 = (computed_fixed_point (=) (fun x -> x /. 2.) 1.) = 0.
+
+let computed_fixed_point_test1 = (computed_fixed_point (=) (fun x -> x *. 10.) 1.) = infinity
+
+
+
+
+
+
+
+
+
+
 type awksub_nonterminals =
   | Expr | Lvalue | Incrop | Binop | Num
 
@@ -26,21 +86,24 @@ let awksub_rules =
 
 let awksub_grammar = Expr, awksub_rules
 
+(* 
+let in_grammar = inRules Expr awksub_rules
 
+let awksub_reachable_rules = clean_rules Lvalue awksub_rules awksub_rules
 
+let more_rules = clean_rules Lvalue awksub_rules awksub_rules;;
 
-let awksub_reachable_rules = filter_reachable (Lvalue, awksub_rules)
+ let filtered_grammar = filter_reachable awksub_grammar;; *)
 
-(*
 let awksub_test0 =
   filter_reachable awksub_grammar = awksub_grammar
 
 let awksub_test1 =
   filter_reachable (Expr, List.tl awksub_rules) = (Expr, List.tl awksub_rules)
-*)
+
 let awksub_test2 =
   filter_reachable (Lvalue, awksub_rules) = (Lvalue, awksub_rules)
-(*
+
 let awksub_test3 =
   filter_reachable (Expr, List.tl (List.tl awksub_rules)) =
     (Expr,
@@ -63,9 +126,7 @@ let awksub_test4 =
       Lvalue, [T "$"; N Expr];
       Incrop, [T "++"];
       Incrop, [T "--"]])
-*)
 
-(*
 type giant_nonterminals =
   | Conversation | Sentence | Grunt | Snore | Shout | Quiet
 
@@ -92,5 +153,3 @@ let giant_test1 =
 
 let giant_test2 =
   filter_reachable (Quiet, snd giant_grammar) = (Quiet, [Quiet, []])
-
-  *)
---
+++ things-i-know.md
@@ -0,0 +1,77 @@
+
+
+# Facts about Me
+
+George Owen
+UCLA Linguistics & Computer Science Major, expected graduation 2022
+3.96 Overall GPA (? check this again because it might be higher now)
+4.00 Major GPA
+
+top links:
+- my website
+- my github
+- my portfolio for art + graphic design + game design
+- my instagram, possibly
+- linktree???
+
+# Things I'm doing right now
+- Juni Learning, Computer Science Instructor
+
+- Juni Learning, Blog Writer
+
+- Enigma at UCLA, Vice President
+
+- Upsilon Pi Epsilon, Social Chair
+
+- Creative Hub, Co-Founder
+
+- UCLA College Scholars Honors Program, Member and Mentor
+
+# Past Experience
+- USN Tech Camp, Lead Instructor
+
+- Camp Whippoorwill, Junior Counselor
+
+- ?????
+
+Skills and Languages which I have taught to others in some official capacity are marked with a star (*)
+
+# Programming Experience
+## Languages
+Python (turtle, pygame, seaborn, matplotlib, pandas, numpy, beautifulsoup, API requests) (*)
+C (*)
+C++ (*)
+Java (*)
+HTML + CSS (*)
+Scratch (*)
+Haskell
+OCaml
+Bash
+git (? possibly a technical skill)
+
+## Classes
+- CS50 through EdX
+- CS31, 32, 33, 35L, 131 at UCLA
+- Ling 119A, 120B, 185A at UCLA
+    (computational linguistics, as well as syntax trees)
+
+# Technical Skills
+Game Design (scratch, python, java, as well as IRL)
+Notion / Markdown
+website design (from scratch, as well as using Tailwind CSS package, Squarespace, Weebly, and Wix)
+- link my website, as well as enigma site if they ask
+figma
+graphic design
+- link my portfolio page (hosted on chub!!!!)
+Adobe Photoshop (*)
+Adobe Flash (*)
+
+SEO Optimization (using clearscope)
+Blogwriting
+
+# Managerial Skills
+Curriculum and Blog Content Development
+Delegating Tasks
+Socially Oriented Game Design (possibly duplicate of game design above, but emphasis on how everything is in-person and coordinating groups of people)
+Public Speaking
+Scheduling and Leading Meetings, iteneraries
